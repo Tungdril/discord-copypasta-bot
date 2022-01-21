88cd17c766ce220a9ab19c881348875ed35e21b6
Laurin Sörensen <49350541+Tungdril@users.noreply.github.com>
2022-01-21 19:39:54 +0100

    changed id

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ client.on('message', msg=>{
 
 //somebody love me pls
 client.on('message', msg=>{
-    if(msg.author.id = 224549612512346112 && msg.content === "I love you"){
-        msg.reply('I love you too!')
+    if(msg.author.id === 224549612512346112 && msg.content == "I love you"){
+        return msg.reply('I love you too!')
     }
     }
 )
